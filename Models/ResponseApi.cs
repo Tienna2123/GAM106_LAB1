00378f82d0f6f237d5929cb26ed78e5131c8c7a5
Tienna2123 <kaidanchoi@gmail.com>
2024-11-19 15:13:24 +0700

    lab 3

---
+++ Models/ResponseApi.cs
@@ -0,0 +1,10 @@
+﻿namespace SeverGame106.Models
+{
+    public class ResponseApi
+    {
+        public bool IsSuccess { get; set; }
+        public string Notification { get; set; }
+        public object Data { get; set; }
+
+    }
+}
